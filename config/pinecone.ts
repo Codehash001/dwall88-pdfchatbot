109cacdebb1548e1227c32c13efae4e6754d67b1
Codehash001 <codehashblockchain@gmail.com>
2023-05-06 21:46:55 +0530

    set namespace as env

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -1,5 +1,8 @@
 
-const PINECONE_INDEX_NAME ='pdf';
-const PINECONE_NAME_SPACE = 'pdfnamespace'; //default namespace
+const indexname = `${process.env.PINECONE_INDEX_NAME}`
+const namespace = `${process.env.PINECONE_NAME_SPACE}`
+
+const PINECONE_INDEX_NAME =indexname;
+const PINECONE_NAME_SPACE = namespace;
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
